d3189825b5b9913aff5a8c1fcb304834901d05fc
ROHIT RAM <ramrohit9701@gmail.com>
2024-05-25 16:44:22 +0530

    index.js

--- 02Variables/index.js
+++ 02Variables/index.js
@@ -1,7 +1,16 @@
-console.log("This repl contains code for the Ultimate JavaScript course video no 2")
-var a = 67 // a contains 67
-console.log(a)
-a = "harry"
-console.log(a)
+console.log("This repl contains code for the Ultimate JavaScript course video no 2");
+let a = 67; // a contains 67
+console.log(a);
+a = "harry";
+console.log(a);
+// start variale name with letter, $, _ but not with number and only these are allowed.
+//The value of a JavaScript variable can be changed during the execution of a program (i.e. during run-time that is why it called Dynamically typed language)
 // let 8harry = 7 // Not allowed this will throw an error
 // let var = 7
+// literal is a constant term, like a number, string, boolean, null, undefined
+b = 8;
+console.log(b);
+var name = "Harry";
+var Name = "Ron";
+console.log(name); // will print "Harry" // do not use name as a variable name, now it is a reserved keyword
+console.log(Name); // will print "Ron"
