00b80bc69bb90e3ae4622293f18f0211b01c67e7
ROHIT RAM <ramrohit9701@gmail.com>
2024-05-27 23:11:15 +0530

    Update index.js

--- 07Conditionals/index.js
+++ 07Conditionals/index.js
@@ -1,5 +1,24 @@
 let a = prompt("Hey whats you age?");
+console.log(typeof a);
 a = Number.parseInt(a); // Converting the string to a number
+console.log(typeof a);
+// If Statement
+/* 
+if(a>17){
+     alert("This is a valid age and you can drive");
+}  
+*/
+// If-Else Statement
+/* 
+if(a>17){
+  alert("This is a valid age and you can drive");
+}  
+else {
+  alert("This is an invalid age and you can't drive);   
+}
+*/
+
+// If-Else-If statement 
 if(a<0){
   alert("This is an invalid age");
 }
@@ -13,5 +32,47 @@ else{
   alert("You can now drive as you are above 18");
 }
 console.log("Done")
+console.log("You can", (a<18? "not drive" :"drive"));
+
+// replit shortcut: 
+// use `alt+ctrl+shift+(` to put the selected statement in brackets()
+// use `alt+ctrl+shift+{` to put statement in brackets {}.
+// shift is used here as () and {} are accessed by pressing shift on keyboard.
+// use `alt+ctrl+[` to put statement in brackets [].
+
 // HomeWork - Explore switch statement and write a basic program in the comments
-console.log("You can", (a<18? "not drive" :"drive"))
+
+// Switch Statement
+const expr = 'Papayas';
+switch (expr) {
+  case 'Oranges':
+    console.log('Oranges are $0.59 a pound.');
+    break;
+  case 'Mangoes':
+  case 'Papayas':
+    console.log('Mangoes and papayas are $2.79 a pound.');
+    // Expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
+
+let x = Number(prompt("Enter the value of x")); // Number() function to convert the string returned by prompt into number so that my input works well.
+const m = 12, n = 4;
+switch (x) {
+  case 1:
+    console.log("Addition", m+n);
+    break;  
+  case 2:
+    console.log("Subtraction", m-n);
+    break;  
+  case 3:
+    console.log("Multiplication", m*n);
+    break;  
+  case 4:
+    console.log("Division", m/n);
+    break;
+  default:
+    console.log("Remainder", m%n);
+    break;
+}
